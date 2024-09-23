72f793ba17d056c2303ec1c97674073d0e6454f6
abhishek8182 <abhishektripathi3196@gmail.com>
2024-09-23 15:11:41 +0530

    adding middleware

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next(); //Allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+  console.log("In another middleware");
+});
+
+app.listen(3000);
